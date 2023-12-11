faae0d2fbadb9be83973402a4a5cdc0b04c23f9c
Beaudette <dylan.beaudette@usda.gov>
2023-12-11 14:16:07 -0800

    Update axidraw-notes.R

--- axidraw/axidraw-notes.R
+++ axidraw/axidraw-notes.R
@@ -4,6 +4,9 @@ library(svglite)
 # https://vpype.readthedocs.io/en/latest/cookbook.html
 # 
 
+## ideas
+# https://mrmrs.cc/writing/pen-plotting-intro/
+
 
 # don't include BBOX rectangle
 svglite(filename = 'e:/temp/electro.svg', width = 7, height = 3.5, bg = 'transparent')
